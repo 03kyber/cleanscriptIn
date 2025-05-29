24d42ac742341fe2d3a9521f51b8fef61a34c768
Kyber <hipermig@gmail.com>
2025-05-29 10:04:01 +0200

    now it works regardless of the username

--- cleanerScript.ps1
+++ cleanerScript.ps1
@@ -1,27 +1,27 @@
 ﻿#Run CCleaner tools
 echo "Running CCleaner cleanup tools..."
 & 'C:\Program Files\CCleaner\CCleaner.exe' /AUTO
-$desktopFiles = Get-ChildItem -Path C:\Users\Propietario\Desktop
-$downloadsFiles = Get-ChildItem -Path C:\Users\Propietario\Downloads
-$savedPicturesFiles = Get-ChildItem -Path 'C:\Users\Propietario\Pictures\Saved Pictures'
+$desktopFiles = Get-ChildItem -LiteralPath "$env:USERPROFILE\Desktop"
+$downloadsFiles = Get-ChildItem -Path "$env:USERPROFILE\Downloads"
+$savedPicturesFiles = Get-ChildItem -Path "$env:USERPROFILE\Pictures\Saved Pictures"
 #Delete "Desktop" files
 echo "Deleting Desktop Files..."
 foreach ($file in $desktopFiles) {
-    del C:\Users\Propietario\Desktop\$file
+    del "$env:USERPROFILE\Desktop\$file"
 }
 #Safe Folder Shortcut Restoration
 $WshShell = New-Object -COMObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Carpeta Segura.lnk")
-$Shortcut.TargetPath = "C:\Users\Propietario\Carpeta Segura"
+$Shortcut.TargetPath = "$env:USERPROFILE\Carpeta Segura"
 $Shortcut.Save()
 #Delete "Downloads" files
 echo "Deleting Downloads Files..."
 foreach ($file in $downloadsFiles) {
-    del C:\Users\Propietario\Downloads\$file
+    del "$env:USERPROFILE\Downloads\$file"
 }
 #Delete "Saved Pictures" Files
 echo "Deleting Saved Pictures Files..."
 foreach ($file in $savedPicturesFiles) {
-    del 'C:\Users\Propietario\Pictures\Saved Pictures\$file'
+    del "$env:USERPROFILE\Pictures\Saved Pictures\$file"
 }
 
